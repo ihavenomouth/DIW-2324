b7a0640385588d7a53fdb91527f8de722b5c5c68
Javier <javmanfer@gmail.com>
2023-10-18 20:32:37 +0200

    Grid y ejercicios resueltos de grid

--- UD2/2b/8 Flex/4 Notificaciones/code.js
+++ UD2/2b/8 Flex/4 Notificaciones/code.js
@@ -1,8 +1,18 @@
 "use strict";
 
-document.querySelector("#btnMostrar").addEventListener("click", ()=> {
+document.querySelector("#btnMostrar").addEventListener("click", () => {
 
 	document.querySelector(".container").classList.toggle("oculto");
 });
 
 
+// Punto de entrada para añadir un alumno
+app.post("/crearAlumno", (request, response) => {
+
+  const nombre = cleanseString(request.body.txtNombre);
+  const email = cleanseString(request.body.txtEmail).toLowerCase();
+  const clave = cleanseString(request.body.txtClave);
+  const curso = cleanseString(request.body.txtCurso);
+  
+  console.log(`Añadir alumno ${nombre}, ${email}, ${curso}`);
+});

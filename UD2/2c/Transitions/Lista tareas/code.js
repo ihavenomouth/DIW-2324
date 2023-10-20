33cb3fa94c611b4154b0a691d195365ef0d84770
Javier <javmanfer@gmail.com>
2023-10-20 20:30:44 +0200

    Minor Fix

--- UD2/2c/Transitions/Lista tareas/code.js
+++ UD2/2c/Transitions/Lista tareas/code.js
@@ -9,12 +9,42 @@
 // MAIN
 /////////////////////
 
+//Objeto vacío
 
-document.querySelectorAll(".información-extra-card > button").forEach(
-	b => {
-		b.addEventListener("click", (e) => {
-			b.classList.toggle("visible");
-			b.nextElementSibling.classList.toggle("visible");
+
+
+// class Persona {
+// 	constructor(nombre, edad) {
+// 		this.nombre = nombre;
+// 		this.edad = edad;
+// 		this.saludar = function () {
+// 			return `Hola, soy ${nombre} y tengo ${edad} años)`;
+// 		};
+// 		this.estado = "inactivo";
+// 	}
+// }
+
+// function Alumno(nombre, edad, curso){
+// 	this.curso = "2º DAW";
+// 	Persona.call(this, nombre, edad);
+// 	this.saludar=function(){
+// 		return `Hola, soy ${this.nombre}, estoy en ${this.curso} y mi estado es ${this.estado}`;
+// 	}
+
+// }
+// Alumno.prototype = Persona;
+
+// let p1 = new Persona("Javier", 42);
+// console.log(p1.saludar());
+
+// let a1 = new Alumno("Juan", 23);
+// console.log(a1)
+// console.log(a1.saludar());
+
+document.querySelectorAll(".lista-tareas-item").forEach(
+	item => {
+		item.addEventListener("click", () => {
+			item.classList.toggle("lista-tareas-item-completado");
 		});
 
 	}
